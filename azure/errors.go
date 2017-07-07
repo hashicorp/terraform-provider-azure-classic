7b826e4912548f5b73e157b375ee2e9881fee16c
Masayuki Morita <minamijoyo@gmail.com>
2017-07-08 00:58:42 +0900

    Fix incorrect error message when using a platform image without storage_service_name parameter

--- azure/errors.go
+++ azure/errors.go
@@ -2,4 +2,4 @@ package azure
 
 import "errors"
 
-var PlatformStorageError = errors.New("When using a platform image, the 'storage' parameter is required")
+var PlatformStorageError = errors.New("When using a platform image, the 'storage_service_name' parameter is required")
